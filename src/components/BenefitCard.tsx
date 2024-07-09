2981f45a42f226bac203f0b5bc7d645e21a8ef68
Davi Borges <daviborges@Davis-MacBook-Pro.local>
2024-07-08 23:06:26 -0300

    pppp

--- src/components/BenefitCard.tsx
+++ src/components/BenefitCard.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 interface BenefitCardProps {
-  color: string;
-  amount: string;
-  label: string;
-  icon: string;
+  color: any;
+  amount: any;
+  label: any;
+  icon: any;
 }
 
 const BenefitCard: React.FC<BenefitCardProps> = ({ color, amount, label, icon }) => {
+  const a = 'a'
+  const b = 'b'
+  console.log(a == b)
   return (
     <div className={`flex flex-col items-center p-4 rounded-lg ${color}`}>
       <i className={`fas ${icon} text-2xl mb-2`}></i>
